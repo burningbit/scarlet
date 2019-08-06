ab0bc5d0fbf232ef687e44aa5866042d471f6b3a
arjun@tradelab.in <arjun@tradelab.in>
2019-08-06 17:20:30 +0530

    SASS initialization

--- src/Components/Login.purs
+++ src/Components/Login.purs
@@ -3,10 +3,11 @@ module Components.Login where
 
 import Prelude
 
-import React (ReactElement, getState)
+import React (ReactElement, component, getProps, getState)
 import React as React
 import React.DOM as D
 import React.DOM.Props as P
+import React.DOM.SVG as DS
 
 loginView :: React.ReactClass {}
 loginView = React.component "LoginView" component
@@ -18,16 +19,45 @@ loginView = React.component "LoginView" component
            , render: render <$> getState this
            }
            where
-             render {loginId, password} = D.div' [passwordInput loginId , passwordInput password]
+             render {loginId, password} = D.div [P.className "scarlet-login-view"] $ [React.createLeafElement loginForm {}]
+
+
+loginForm :: React.ReactClass {}
+loginForm = React.component "LoginForm" component
+  where
+    component this = 
+      pure { state: { loginId: ""
+                    , password: ""
+                    }
+           , render: render <$> getState this
+           }
+            where 
+              render {loginId, password} =  D.div [P.className "scarlet-login-view-form"] [loginIdContainer loginId , passwordInputContainer password]
                 where
-                  loginIdContainer loginId' = React.createElement inputContainer {} []
-                  loginIdInput loginId'' = D.input [P.className "scarlet-login-view-loginid-input"]
-                  passwordInput password' = D.input [P.className "scarlet-login-view-password-input"]
+                  loginIdContainer loginId' = React.createLeafElement inputContainer {value: loginId', _type: "text", label: "Username", _id : "login_id_label"}
+                  passwordInputContainer password' = React.createLeafElement inputContainer {value: password', _type: "password", label: "Password", _id: "password_label"}
 
 
--- inputContainer :: Array ReactElement React.ReactClass {}
+inputContainer :: React.ReactClass {value :: String, _type :: String, label :: String, _id :: String}
 inputContainer = React.component "InputContainer" component
   where
     component this = 
       pure { state: {}
+           , render: render <$> getProps this
            }
+           where
+             render {value, _type, label, _id} = 
+              D.div [ P.className "scarlet-login-view-textbox-container" ] 
+                      [ D.label [ P.className "scarlet-login-view-textbox-label", P.htmlFor _id ] [ imageIcon _id ]
+                      , D.input [ P.className "scarlet-login-view-textbox"
+                                , P._type _type
+                                , P._id _id]
+                      ]
+
+imageSource :: String -> String
+imageSource "login_id_label" = "https://www.svgrepo.com/show/794/male-user.svg"
+imageSource "password_label" = "https://www.svgrepo.com/show/955/padlock.svg"
+imageSource _ = "https://www.svgrepo.com/show/221473/lost-items-missing.svg"
+
+imageIcon :: String -> ReactElement
+imageIcon src = D.img [P.className "scarlet-icon-20", P.src (imageSource src)]
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,185 @@
 'use strict';
 
 const path = require('path');
+
 const webpack = require('webpack');
 
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
+const isWebpackDevServer = process.argv.some(a => path.basename(a) === 'webpack-dev-server');
 
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isWatch = process.argv.some(a => a === '--watch');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
+const plugins =
+  isWebpackDevServer || !isWatch ? [] : [
+    function(){
+      this.plugin('done', function(stats){
+        process.stderr.write(stats.toString('errors-only'));
+      });
+    }
+  ]
+;
 
 module.exports = {
-	mode: 'development',
+  devtool: 'eval-source-map',
 
-	entry: './src/App',
+  devServer: {
+    contentBase: __dirname,
+    port: 4008,
+    stats: 'errors-only'
+  },
 
-	output: {
-		path: __dirname,
-		pathinfo: true,
-		filename: 'bundle.js',
-		devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
-	},
+  entry: './src/App',
 
-	// plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+  output: {
+    path: __dirname,
+	pathinfo: true,
+	publicPath: __dirname,
+    filename: 'bundle.js'
+  },
 
-	module: {
-		rules: [
-		  {
-			test: /\.purs$/,
-			loader: 'purs-loader',
-			exclude: /node_modules/,
-			query: {
-			  src: [
-				'bower_components/purescript-*/src/**/*.purs',
-				'src/**/*.purs'
-			  ],
-			  bundle: false,
-			  psc: 'psa',
-			  pscIde: false
-			}
-		  }
-		]
-	},
-	
-	resolve: {
-		modules: [ 'node_modules', 'bower_components' ],
-		extensions: [ '.purs', '.js']
-	},
+  module: {
+    rules: [
+					{
+				test: /\.scss$/,
+				use: [
+					"style-loader", // creates style nodes from JS strings
+					"css-loader", // translates CSS into CommonJS
+					"sass-loader" // compiles Sass to CSS, using Node Sass by default
+				]
+			},
+      {
+        test: /\.purs$/,
+        use: [
+          {
+            loader: 'purs-loader',
+            options: {
+              src: [
+                'bower_components/purescript-*/src/**/*.purs',
+                'src/**/*.purs'
+              ],
+              bundle: false,
+              psc: 'psa',
+              watch: isWebpackDevServer || isWatch,
+              pscIde: false
+            }
+          }
+        ]
+      },
+    ]
+  },
 
+  resolve: {
+    modules: [ 'node_modules', 'bower_components' ],
+    extensions: [ '.purs', '.js']
+  },
 
-	devServer: {
-		contentBase: '.',
-		port: 4008,
-		stats: 'errors-only'
-	},
-
-	plugins: [
-		new webpack.LoaderOptionsPlugin({
-		  debug: true
-		})
-	  ]
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+	})
+  ].concat(plugins)
 };
+
+// 'use strict';
+
+// const path = require('path');
+// const webpack = require('webpack');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// /*
+//  * SplitChunksPlugin is enabled by default and replaced
+//  * deprecated CommonsChunkPlugin. It automatically identifies modules which
+//  * should be splitted of chunk by heuristics using module duplication count and
+//  * module category (i. e. node_modules). And splits the chunks…
+//  *
+//  * It is safe to remove "splitChunks" from the generated configuration
+//  * and was added as an educational example.
+//  *
+//  * https://webpack.js.org/plugins/split-chunks-plugin/
+//  *
+//  */
+
+// // const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// /*
+//  * We've enabled HtmlWebpackPlugin for you! This generates a html
+//  * page for you when you compile webpack, which will make you start
+//  * developing and prototyping faster.
+//  *
+//  * https://github.com/jantimon/html-webpack-plugin
+//  *
+//  */
+
+// module.exports = {
+// 	mode: 'development',
+
+// 	entry: './src/App',
+
+// 	output: {
+// 		path: path.resolve(__dirname, 'dist'),
+// 		publicPath: '/dist/',
+// 		pathinfo: true,
+// 		filename: 'bundle.js',
+// 		devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
+// 	},
+
+// 	// plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.scss$/,
+// 				use: [
+// 					"style-loader", // creates style nodes from JS strings
+// 					"css-loader", // translates CSS into CommonJS
+// 					"sass-loader" // compiles Sass to CSS, using Node Sass by default
+// 				]
+// 			},
+// 			{
+// 				test: /\.purs$/,
+// 				loader: 'purs-loader',
+// 				exclude: /node_modules/,
+// 				query: {
+// 					src: [
+// 					'bower_components/purescript-*/src/**/*.purs',
+// 					'src/**/*.purs'
+// 					],
+// 					bundle: false,
+// 					psc: 'psa',
+// 					pscIde: false
+// 				}
+// 			}
+// 		]
+// 	},
+	
+// 	resolve: {
+// 		modules: [ 'node_modules', 'bower_components' ],
+// 		extensions: [ '.purs', '.js', '.scss']
+// 	},
+
+
+// 	devServer: {
+// 		contentBase: path.join(__dirname, 'dist'),
+// 		port: 4008,
+// 		stats: 'errors-only'
+// 	},
+
+// 	plugins: [
+// 		new webpack.LoaderOptionsPlugin({
+// 		  debug: true
+// 		}),
+// 		new MiniCssExtractPlugin({
+//             // Options similar to the same options in webpackOptions.output
+//             // both options are optional
+//             filename: "style.css",
+// 			chunkFilename: "[name].css",
+// 			output: {
+// 				filename: "[name]-min.js",
+// 				path: path.resolve(__dirname, "dist"),
+// 				publicPath: "/static/react/dist/"
+// 			}
+//         })
+// 	  ]
+// };
 
 
 
